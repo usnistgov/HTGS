765b14be794b69e726736e5bcb7c7ccbd6d21ea1
Tim Blattner <timothy.blattner@nist.gov>
2017-02-10 16:27:58 -0500

    - Added main function to test changes to API

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+
+#include <htgs/api/TaskGraph.hpp>
+
+int main()
+{
+
+  htgs::TaskGraph<htgs::VoidData, htgs::VoidData> *taskGraph = new htgs::TaskGraph<htgs::VoidData, htgs::VoidData>();
+  std::cout << "Hello world" << std::endl;
+
+
+}
